b523fb42192c4c418ba9bb375bfe29d480d87456
Jesus-Cries <16zeichen@gmail.com>
2021-10-25 16:39:09 +0200

    Add mui

---
+++ frontend/src/components/Example/Example.tsx
@@ -0,0 +1,23 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    root: {
+        
+    },
+});
+
+interface Props {
+    
+}
+
+const Example: React.FC<Props> = () => {
+const classes = useStyles();
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Example;
