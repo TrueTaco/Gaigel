fb536072f3324a6b7882d8c7a3d0a175edf88f0b
Jesus-Cries <16zeichen@gmail.com>
2022-01-05 18:59:35 +0100

    ADD cors headers

--- server.js
+++ server.js
@@ -9,12 +9,19 @@ const io = socketIO(server)
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
+// Header settings
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "http://localhost:5000"); // In Production
+    res.header("Access-Control-Allow-Origin", "*"); // In Development
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use("/", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
-
 
 let interval;
 
@@ -24,6 +31,7 @@ io.on("connection", (socket) => {
 
     interval = setInterval(() => {
         socket.emit("onConnectionMessage", "Hello Client");
+        console.log("Sending a message");
     }, 1000)
 
     socket.on("disconnect", () => {
