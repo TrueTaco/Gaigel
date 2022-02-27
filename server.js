6964f38e534067d19c58038d1b025e2c685fb4d3
Michel Depcik <80948356+TrueTaco@users.noreply.github.com>
2022-02-27 15:31:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 server.listen(port, () => {
     console.log(`Server started on port ${port}`);
